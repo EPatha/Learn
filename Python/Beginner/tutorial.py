86ed23b48a92aa9a7c73c47297cc484d96c17255
Ephesians Prismaranatha <ephesiansprismaranatha@gmail.com>
2025-01-20 23:01:42 +0700

    learn w3school tutorial python

---
+++ Python/Beginner/tutorial.py
@@ -0,0 +1,29 @@
+#Comment block print ("Hello World")
+import sys
+print(sys.version)
+#Comment block
+if 5 > 2:
+ print("Five is great than two")
+ if 5 > 2:
+    print("Five is great than two")#Comment block
+ x=5
+ y="hello world"
+ X=6
+ print (X)
+ """
+ sadasdad
+ asdadas
+ adasd
+ """
+#yang terakhir yang dibaca
+x=4
+x="ep"
+print(x)
+
+x=str(3)
+y=int(3)
+z=float(3)
+print (x,y,z)
+print(type(x),type(y),type(z))
+ep='ep'
+print(ep) 
