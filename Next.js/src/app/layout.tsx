f9408f65d651b408d33ecc13a9f3555f1d6522d6
Ephesians Prismaranatha <ephesiansprismaranatha@gmail.com>
2025-05-05 21:07:33 +0700

    initial commit learn next.js

---
+++ Next.js/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mekanikace",
+  description: "Mekanikace is IT consulting company",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${interFont.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
